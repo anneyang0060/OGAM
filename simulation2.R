1e760c6cae6f8085a74777472b2fe1ecd0596743
anneyang0060 <102971671+anneyang0060@users.noreply.github.com>
2022-11-26 22:51:51 +0800

    Update simulation2.R

--- simulation2.R
+++ simulation2.R
@@ -35,7 +35,7 @@ source('FNS/FNS_DataGene_Simu2.R')
 load('res/sim2/online_constants_for_bandwidths.Rdata')
 for(L in c(3,5, 10))
 {
-  Mcl<-50
+  Mcl<-100
   cl<-makeCluster(Mcl)
   registerDoParallel(cl)
   oln<- foreach(sd=1:100) %dopar%{
