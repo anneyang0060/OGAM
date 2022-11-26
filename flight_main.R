1b1c4d6dbdd6dfe4a0bf85da4ceccc0f385c6b32
anneyang0060 <102971671+anneyang0060@users.noreply.github.com>
2022-11-26 22:11:37 +0800

    Update flight_main.R

--- flight_main.R
+++ flight_main.R
@@ -1,4 +1,4 @@
-setwd('/home/yangy/OGAM_code_and_data')
+setwd('.../OGAM')
 source('FNS/FNS_SmoBack.R')
 
 #### parameter
@@ -7,10 +7,10 @@ source('FNS/FNS_SmoBack.R')
   d <- 2
   m <- 25 # No evalpoints
   eval_vec <- seq(0.05, 0.95, length.out = m)
-  
   Max_iter <- 10
-  
-  K_band <- 600
+  N <- 0
+  pd1 <- 1  
+  K_band <- 300
   G <- rep(1,d)
   L_theta <- 5; L_sigma <- 5
   L <- 10
@@ -25,7 +25,6 @@ source('FNS/FNS_SmoBack.R')
   beta0_store <-c()
   beta_store <-c()
   band <- c()
-  band_select <- FALSE
   load('res/flight/online_constants_for_bandwidths.Rdata')
   start <- 0
   
@@ -47,23 +46,84 @@ source('FNS/FNS_SmoBack.R')
       X <- cbind(data$CRSDepTime,data$HistDelayRate)
       y <- data$Delayed
       n <- length(y)
+      N <- N+n
 
       # delta
       delta_inner <- 0.01*(K<=10)+0.001*(10<K&K<=30)+1e-04*(K>30)
       delta_outer <- delta_inner
       
-      tt0 <- Sys.time()
-      # online gam
-      ogam(K, X, y, n, m, delta_inner, delta_outer, Max_iter, band_select, K_band, C1=C1[,min(K,K_band)], L=L)
-      tt1 <- Sys.time()
+      t0<-Sys.time()
       
+      if(K==1){
+        
+        ## some initial values
+        h <- rep(1,d)
+        ## stored statistics for the main regression
+        U_ <- array(0, dim = c(pd1*d+1,m^d,L))
+        V_ <- array(0, dim = c(pd1*d+1,pd1*d+1,m^d,L)) 
+        Q_ <- array(0, dim = c(pd1*d+1,pd1*d+1,m^d,L))
+        if(link!='identity'){
+          R_ <- array(0, dim = c(pd1*d+1,pd1*d+1,pd1*d+1,m^d,L))
+        }else{
+          R_ <- 0
+        }
+      }  
+      
+      # compute bandwidth and candidates
+      {
+        Ch <- C1[,min(K,K_band)]
+        h <- sapply(1:d, function(i){min(Ch[i]*N^(-1/5),h[i])})
+        eta <- sapply(1:L, function(l){((L-l+1)/L)^(1/5) * h}) #dim = d*L
+      }
+      
+      # initial parametric estimate and combination rule
+      if(K==1){
+        
+        initial_res <- initial(X,y,h,eval_vec,pd1)
+        beta0_est <- initial_res$beta0; beta_est <- initial_res$beta
+        
+        idx <- 1:L
+        centrds <- eta
+        
+      }else{
+        
+        idx<-sapply(1:L,function(l){
+          which.min(abs(eta[1,l] - centrds[1,]))
+        })
+        for(i in 1:d){
+          centrds[i,] <- (centrds[i,idx] * (N-n[K]) + eta[i,] * n[K]) / N
+        }
+        
+      }
+      
+      # backfitting
+      {
+        res_beta <- backfitting(beta0_est, beta_est,U_[,,idx[1]],V_[,,,idx[1]],Q_[,,,idx[1]],R_[,,,,idx[1]], n, N, h, pd1)
+        if(res_beta$delta < delta_outer){
+          beta0_est <- res_beta$beta0; beta_est <- res_beta$beta
+          print(paste0('backfitting modified : delta=', res_beta$delta))
+        }
+        rm(res_beta)
+      }
+      # print('backfitting OK')
+      
+      # update statistics
+      {
+        res_update <- update_stats(U_,V_,Q_,R_, beta0_est, beta_est, eta, idx, n, N, pd1, L)
+        U_ <- res_update$U_
+        V_ <- res_update$V_
+        Q_ <- res_update$Q_
+        R_ <- res_update$R_
+        rm(res_update)
+      }
+      t1<-Sys.time()      
       # store
       band<-cbind(band,h)
-      time <-c(time, as.numeric(tt1-tt0,unit='secs'))
+      time <-c(time, as.numeric(t1-t0,unit='secs'))
       beta0_store <- c(beta0_store,beta0_est)
       beta_store <- cbind(beta_store,beta_est[,1:d])
 
-      print(paste0('K=',K,', time=', round(difftime(tt1, tt0, units = 'secs'),3)))
+      print(paste0('K=',K,', time=', round(difftime(t1, t0, units = 'secs'),3)))
       par(mfrow=c(1,2))
       plot(beta_store[,2*K-1],type='l')
       plot(beta_store[,2*K], type='l')
@@ -87,7 +147,7 @@ sub_streams <- c(1,seq(10,50,10),seq(100,1000,100), seq(1500,2500,500),3283)
   band <- c()
   X <- c(); y <- c()
   band_select <- FALSE
-  load('res/flight/online_constants_for_bandwidths.Rdata')
+  load('res/flight/batch_constants_for_bandwidths.Rdata')
   start <- 0
   
   for(year in 1996:2004){
@@ -108,6 +168,7 @@ sub_streams <- c(1,seq(10,50,10),seq(100,1000,100), seq(1500,2500,500),3283)
       X <- rbind(X, cbind(data$CRSDepTime,data$HistDelayRate))
       y <- c(y, data$Delayed)
       n <- length(y)
+      N <- n+N
       
       if(K %in% sub_streams){
 
@@ -116,13 +177,43 @@ sub_streams <- c(1,seq(10,50,10),seq(100,1000,100), seq(1500,2500,500),3283)
         delta_outer <- delta_inner
         
         # online gam
-        tt0 <- Sys.time()
-        gam(1, X, y, n, delta_inner, delta_outer, band_select, K_band, C1=C1[,which(sub_streams==min(K,K_band))])
-        tt1 <- Sys.time()
+        t0 <- Sys.time()
+        {
+          ## some initial values
+          h <- rep(1,d)
+          ## stored statistics for the main regression
+          U_ <- array(0, dim = c(pd1*d+1,m^d,L))
+          V_ <- array(0, dim = c(pd1*d+1,pd1*d+1,m^d,L)) 
+          Q_ <- array(0, dim = c(pd1*d+1,pd1*d+1,m^d,L))
+          if(link!='identity'){
+            R_ <- array(0, dim = c(pd1*d+1,pd1*d+1,pd1*d+1,m^d,L))
+          }else{
+            R_ <- 0
+          }
+        }  
+        
+        # compute bandwidth and candidates
+        Ch <- C1[,min(K,K_band)]
+        h <- Ch*N^(-1/5)
+        
+        # initial parametric estimate and combination rule
+        initial_res <- initial(X,y,h,eval_vec,pd1)
+        beta0_est <- initial_res$beta0; beta_est <- initial_res$beta
+        
+        # backfitting
+        {
+          res_beta <- backfitting(beta0_est, beta_est,U_[,,1],V_[,,,1],Q_[,,,1],R_[,,,,1], N, N, h, pd1)
+          if(res_beta$delta < delta_outer){
+            beta0_est <- res_beta$beta0; beta_est <- res_beta$beta
+            print(paste0('backfitting modified : delta=', res_beta$delta))
+          }
+          rm(res_beta)
+        }
+        t1 <- Sys.time()
         
         # store
         band<-cbind(band,h)
-        time <-c(time, as.numeric(tt1-tt0,unit='secs'))
+        time <-c(time, as.numeric(t1-t0,unit='secs'))
         beta0_store <- c(beta0_store,beta0_est)
         beta_store <- cbind(beta_store,beta_est[,1:d])
         
