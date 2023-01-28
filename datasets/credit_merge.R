53a47e11d6996b2b47672e4df3eb5fb537e61738
anneyang0060 <102971671+anneyang0060@users.noreply.github.com>
2023-01-28 21:01:31 +0800

    Update credit_merge.R

--- datasets/credit_merge.R
+++ datasets/credit_merge.R
@@ -1,1 +1,11 @@
+setwd('.../OGAM/datasets/credit')
 
+file1 <- 'P2P_Macro_Data_processed_part1.Rdata'
+file2 <- 'P2P_Macro_Data_processed_part2.Rdata'
+load(file1); df1 <- df
+load(file2); df2 <- df
+file.remove(file1)
+file.remove(file2)
+
+df <- rbind(df1, df2)
+save(df,file = 'P2P_Macro_Data_processed.Rdata')
